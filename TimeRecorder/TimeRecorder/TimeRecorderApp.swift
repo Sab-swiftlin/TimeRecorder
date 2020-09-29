79328563223d81b6e3f5a80d9912c4e27ff00697
Sab-swiftlin <nukunuku23@gmail.com>
2020-09-29 23:08:00 +0900

    プロジェクトファイルを作成

---
+++ TimeRecorder/TimeRecorder/TimeRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeRecorderApp.swift
+//  TimeRecorder
+//
+//  Created by Haruki Saburi on 2020/09/29.
+//
+
+import SwiftUI
+
+@main
+struct TimeRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
