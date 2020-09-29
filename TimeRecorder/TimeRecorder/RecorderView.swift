4ee52e591e72597d907eff9c397a70ab7f7bec19
Sab-swiftlin <nukunuku23@gmail.com>
2020-09-29 23:26:46 +0900

    打刻用画面のファイルを追加

---
+++ TimeRecorder/TimeRecorder/RecorderView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct RecorderView: View {
+    var body: some View {
+        Text("RecorderView")
+    }
+}
+
+struct RecorderView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecorderView()
+    }
+}
