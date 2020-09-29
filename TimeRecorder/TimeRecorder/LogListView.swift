4fee3c4839a46de3a5d39dab4fe1fd5d03e4ad5b
Sab-swiftlin <nukunuku23@gmail.com>
2020-09-29 23:28:08 +0900

    記録閲覧用画面のファイルを追加

---
+++ TimeRecorder/TimeRecorder/LogListView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct LogListView: View {
+    var body: some View {
+        Text("LogListView")
+    }
+}
+
+struct LogListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogListView()
+    }
+}
