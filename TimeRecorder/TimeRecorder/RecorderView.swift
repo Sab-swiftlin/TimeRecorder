682b960d92c57521cb32663b0757b40a3b88f22c
Sab-swiftlin <nukunuku23@gmail.com>
2020-10-02 00:06:34 +0900

    記録画面のレイアウトを作成

--- TimeRecorder/TimeRecorder/RecorderView.swift
+++ TimeRecorder/TimeRecorder/RecorderView.swift
@@ -2,12 +2,51 @@ import SwiftUI
 
 struct RecorderView: View {
     var body: some View {
-        Text("RecorderView")
+        VStack {
+            Text("2020/10/01")
+            Text("(木)")
+            Text("10:10")
+                .font(.system(size: 60, weight: .bold, design: .default))
+            Spacer().frame(width: 0, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            HStack {
+                Text("出勤").foregroundColor(.gray)
+                Text("10:10")
+                    .foregroundColor(.gray)
+            }
+            HStack {
+                Text("休入").foregroundColor(.gray)
+                Text("12:10").foregroundColor(.gray)
+            }
+            HStack {
+                Text("休出")
+                    .foregroundColor(.gray)
+                Text("13:10").foregroundColor(.gray)
+            }
+            HStack {
+                Text("退勤")
+                    .foregroundColor(.gray)
+                Text("20:10").foregroundColor(.gray)
+            }
+            Spacer().frame(width: 0, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Button(action: {
+                print("Button Tapped")
+            }) {
+                Text("退勤")
+                    .padding(.horizontal, 50)
+                    .padding(.vertical, 20)
+            }
+            .foregroundColor(.white)
+            .background(Color.pink)
+        }
     }
 }
 
 struct RecorderView_Previews: PreviewProvider {
     static var previews: some View {
-        RecorderView()
+        Group {
+            RecorderView()
+            RecorderView()
+                .previewDevice("iPhone 11")
+        }
     }
 }
